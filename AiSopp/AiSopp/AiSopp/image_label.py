5a3c8676b41fbd003401221ef45633e0642b99b8
Grantern73 <grantern@gmail.com>
2017-04-22 21:30:31 +0200

    Clean up image_label and web.config fix

--- AiSopp/AiSopp/AiSopp/image_label.py
+++ AiSopp/AiSopp/AiSopp/image_label.py
@@ -1,37 +1,4 @@
-"""
-Preparing model:
- - Install bazel ( check tensorflow's github for more info )
-    Ubuntu 14.04:
-        - Requirements:
-            sudo add-apt-repository ppa:webupd8team/java
-            sudo apt-get update
-            sudo apt-get install oracle-java8-installer
-        - Download bazel, ( https://github.com/bazelbuild/bazel/releases )
-          tested on: https://github.com/bazelbuild/bazel/releases/download/0.2.0/bazel-0.2.0-jdk7-installer-linux-x86_64.sh
-        - chmod +x PATH_TO_INSTALL.SH
-        - ./PATH_TO_INSTALL.SH --user
-        - Place bazel onto path ( exact path to store shown in the output)
-- For retraining, prepare folder structure as
-    - root_folder_name
-        - class 1
-            - file1
-            - file2
-        - class 2
-            - file1
-            - file2
-- Clone tensorflow
-- Go to root of tensorflow
-- bazel build tensorflow/examples/image_retraining:retrain
-- bazel-bin/tensorflow/examples/image_retraining/retrain --image_dir /path/to/root_folder_name  --output_graph /path/output_graph.pb --output_labels /path/output_labels.txt --bottleneck_dir /path/bottleneck
-** Training done. **
-For testing through bazel,
-    bazel build tensorflow/examples/label_image:label_image && \
-    bazel-bin/tensorflow/examples/label_image/label_image \
-    --graph=/path/output_graph.pb --labels=/path/output_labels.txt \
-    --output_layer=final_result \
-    --image=/path/to/test/image
-For testing through python, change and run this code.
-"""
+
 import os
 import numpy as np
 import tensorflow as tf
@@ -80,10 +47,5 @@ def run_inference_on_image(image):
             prediction.append(labels[node_id].replace("b'","").replace("\\n","").replace("'","").replace("\\r", ""))
             prediction.append(predictions[node_id]*100)
             predictions_list.append(prediction)
-            #human_string = labels[node_id]
-            #score = predictions[node_id]
-            #predictions_list.append('%s (percentage = %.2f)' % (human_string.replace("b'","").replace("\\n","").replace("'",""), score*100))
 
-        #answer = labels[top_k[0]]
-        #return answer
         return predictions_list
